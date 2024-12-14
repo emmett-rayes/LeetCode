cc47251e4c86031a2224553d41ec79635a87214d
Emmett Rayes <emmett.rayes@icloud.com>
2024-12-14 10:01:15 +0100

    Add valid_palindrome.py

---
+++ valid_palindrome.py
@@ -0,0 +1,14 @@
+def is_palindrome(s: str) -> bool:
+    l = 0
+    r = len(s) - 1
+    while l < r:
+        if not s[l].isalnum():
+            l += 1
+        elif not s[r].isalnum():
+            r -= 1
+        elif s[l].lower() != s[r].lower():
+            return False
+        else:
+            l += 1
+            r -= 1
+    return True
