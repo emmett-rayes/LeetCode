113dafd8477d25fb8da83febaf36aed67ffce997
Emmett Rayes <emmett.rayes@icloud.com>
2024-12-15 20:54:25 +0100

    Add same_binary_tree.py

---
+++ python/same_binary_tree.py
@@ -0,0 +1,18 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def is_same_tree(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+    if p is None and q is None:
+        return True
+    if p is None or q is None:
+        return False
+    if p.val != q.val:
+        return False
+    return is_same_tree(p.left, q.left) and is_same_tree(p.right, q.right)
